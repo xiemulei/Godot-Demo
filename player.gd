902c7a9addb438aaeb77fd6d9adf9daf6b2eee28
xiemulei <xiemulei@gmail.com>
2026-01-23 08:02:25 +0800

    06:滑墙

--- player.gd
+++ player.gd
@@ -6,6 +6,7 @@ enum State {
 	JUMP,
 	FALL,
 	LANDING,
+	WALL_SLIDING,
 }
 
 const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
@@ -17,10 +18,12 @@ const JUMP_VELOCITY := -350.0
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var graphics: Node2D = $Graphics
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var jump_request_timer: Timer = $JumpRequestTimer
+@onready var hand_checker: RayCast2D = $Graphics/HandChecker
+@onready var foot_checker: RayCast2D = $Graphics/FootChecker
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
@@ -42,6 +45,9 @@ func tick_physics(state: State, delta: float) -> void:
 			move(default_gravity, delta)
 		State.LANDING:
 			move(default_gravity, delta)
+		State.WALL_SLIDING:
+			move(default_gravity / 3, delta)
+			graphics.scale.x = get_wall_normal().x
 	
 	is_first_tick = false
 
@@ -52,7 +58,7 @@ func move(gravity: float, delta: float) -> void:
 	velocity.y += gravity * delta
 	
 	if not is_zero_approx(direction):
-		sprite_2d.flip_h = direction < 0
+		graphics.scale.x = -1 if direction < 0 else +1
 	
 	move_and_slide()
 
@@ -71,7 +77,6 @@ func get_next_state(state: State) -> State:
 				return State.FALL
 			if not is_still:
 				return State.RUNNING
-				
 		State.RUNNING:
 			if not is_on_floor():
 				return State.FALL
@@ -82,10 +87,19 @@ func get_next_state(state: State) -> State:
 				return State.FALL
 		State.FALL:
 			if is_on_floor():
-				return State.LANDING
+				return State.LANDING if is_still else State.RUNNING
+			if is_on_wall() and hand_checker.is_colliding() and foot_checker.is_colliding():
+				return State.WALL_SLIDING
 		State.LANDING:
+			if not is_still:
+				return State.RUNNING
 			if not animation_player.is_playing():
 				return State.IDLE
+		State.WALL_SLIDING:
+			if is_on_floor():
+				return State.IDLE
+			if not is_on_wall():
+				return State.FALL
 									
 	return state
 	
@@ -109,5 +123,7 @@ func transition_state(from: State, to: State) -> void:
 				coyote_timer.start()
 		State.LANDING:
 			animation_player.play("landing")
+		State.WALL_SLIDING:
+			animation_player.play("wall_sliding")
 	
 	is_first_tick = true
